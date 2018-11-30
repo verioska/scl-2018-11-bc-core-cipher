31172594ecac73c7a276a56e762e30b29c50391d
verioska <rverioska01@gmail.com>
2018-11-30 08:57:34 -0300

    Arreglando mi elemnto p index.html

--- cipher.js
+++ cipher.js
@@ -1,8 +1,51 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (strng,offset1) => {
+    offset1 = parseInt(offset1)
+    let total=[];    //Vvariable local
+    for(i=0; i< strng.length;i++) {
+
+
+
+
+      let ascii= strng.charCodeAt(i);
+      if(ascii>=65 && ascii<=90)
+      {                //variable local
+        let resultado=(ascii-65+offset1) %26+65;
+        let resultadofinal=String.fromCharCode(resultado);
+        total.push(resultadofinal);
+      }
+
+      else {
+        alert("caracter no valido")
+        return ;
+      }
+    }
+    let total1Joined=total.join("");
+    return total1Joined;
   },
-  decode: () => {
-    /* Acá va tu código */
+
+
+
+  decode: (strng1,offset1) => {
+
+    let total=[];    //Vvariable local
+    for(i=0; i< strng1.length;i++) {
+
+
+      let ascii= strng1.charCodeAt(i);
+      if(ascii>=65 && ascii<=90){
+        let resultado=(ascii-90-offset1) %26+90 ;
+        let resultadofinal=String.fromCharCode(resultado);
+        total.push(resultadofinal);
+      }
+
+
+      else {
+
+        return;
+      }
+    }
+    let total1Joined=total.join("");
+    return total1Joined;
   }
 };
--- index.js
+++ index.js
@@ -1,1 +1,82 @@
-/* Acá va tu código */
+//window.onload = ()=> {
+
+
+const botonComenzar1 = document.getElementById("botonComenzar1");
+const botonComenzar2 = document.getElementById("botonComenzar2");
+const main = document.getElementById("main");
+
+botonComenzar1.addEventListener("click",function(){
+
+
+  main.innerHTML=`<p align='center'> Ingresa tu mensaje que deseas cifrar<p>
+  <p align='center'> NOTA: No debes ingresar letras minuscula, ni caracteres, ni la letra ñ<p>
+  <p align='center'><textarea id="mensaje" rows="5" cols="70"></textarea><p>
+  <p align='center'> Ingrese el numero de recorrido<p>
+  <p align='center'> NOTA: No debes ingresar numeros negativos<p>
+  <p align='center'><textarea id="offset" rows="1" cols="3"></textarea><p>
+
+
+
+  <center>
+  <button id="botonOk" onclick="next()">siguiente</button>
+
+
+
+  <button id="buttonBack" onclick=location=URL> Volver al inicio </button>
+  <center>`;
+
+
+});
+function next(){
+  let strng = document.getElementById("mensaje").value;
+//  console.log(strng);
+  let offset1 = document.getElementById("offset").value;
+  //console.log(offset1)
+
+
+  main.innerHTML=`<p align='center'> Tu mensaje codificado es: <br><br><br>${window.cipher.encode(strng,offset1)} <br><br><br><p>
+  <center>
+
+
+  <button id="buttonBack" onclick=location=URL> Volver al inicio </button>
+  <center>`;
+
+
+
+
+};
+
+
+botonComenzar2.addEventListener("click",function(){
+
+  main.innerHTML=`<p align='center'>Ingresa tu mensaje que deseas decodificar<p>
+  <p align='center'> NOTA: No debes ingresar letras minuscula, ni caracteres, ni la letra ñ<p>
+  <p align='center'><textarea id="mensajedecodificado" rows="5" cols="70"></textarea><p>
+  <p align='center'> Ingrese el numero de recorrido<p>
+  <p align='center'> NOTA: No debes ingresar numeros negativos<p>
+  <p align='center'><textarea id="offset1" rows="1" cols="3"></textarea><p>
+
+
+
+  <center>
+  <button id="botonOk" onclick="next1()">siguiente1</button>
+
+  <button id="buttonBack" onclick=location=URL> Volver al inicio </button>
+  <center>`;
+
+
+
+});
+function next1(){
+  let strng1 = document.getElementById("mensajedecodificado").value;
+  //console.log(strng1);
+  let offset1 = document.getElementById("offset1").value;
+  console.log(window.cipher.decode(strng1,offset1));
+
+  main.innerHTML=`<p align='center'> Tu mensaje decodificado es:<br><br><br>${window.cipher.decode(strng1,offset1)}<p>
+
+  <center>
+  <button id="buttonBack" onclick=location=URL> Volver al inicio </button>
+  <center>`;
+
+}
