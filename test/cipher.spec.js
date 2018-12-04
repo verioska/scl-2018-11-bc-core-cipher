5dd25fba4002e124a9473ab6c7a0e3723cd74bc4
verioska <rverioska01@gmail.com>
2018-12-04 10:15:10 -0300

    TEST ACOMODADO

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,8 +10,12 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
-  });
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
+       assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ",33),"HIJKLMNOPQRSTUVWXYZABCDEFG");
+     });
+
+
+});
 
   describe('cipher.decode', () => {
 
@@ -19,7 +23,8 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+      assert.equal(cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG" ,33),"ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    });
   });
-
 });
